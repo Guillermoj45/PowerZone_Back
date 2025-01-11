dfed4bff025ee2a1217fd2a5a433864f9dd94194
Guillermo <guillermoremesa@gmail.com>
2025-01-11 18:06:37 +0100

    dockerfile hecho

--- src/main/java/com/actividad_10/powerzone_back/PowerZoneBackApplication.java
+++ src/main/java/com/actividad_10/powerzone_back/PowerZoneBackApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PowerZoneBackApplication {
 
     public static void main(String[] args) {
+        System.out.println("hola: " + args.length);
         for (String arg : args) {
             System.out.println("hola :" + arg);
         }
