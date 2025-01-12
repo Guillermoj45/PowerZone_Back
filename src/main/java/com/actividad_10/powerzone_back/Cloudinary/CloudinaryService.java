a06c84a1a9e0e4e1f71d789d179544e53d3284dd
Guillermo <guillermoremesa@gmail.com>
2025-01-12 15:36:27 +0100

    Cloudinary hecho

--- src/main/java/com/actividad_10/powerzone_back/Cloudinary/CloudinaryService.java
+++ src/main/java/com/actividad_10/powerzone_back/Cloudinary/CloudinaryService.java
@@ -1,5 +1,5 @@
 package com.actividad_10.powerzone_back.Cloudinary;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
@@ -9,15 +9,18 @@ import java.io.IOException;
 import java.util.Map;
 
 @Service
+@AllArgsConstructor
 public class CloudinaryService {
 
     private final Cloudinary cloudinary;
 
-    @Autowired
-    public CloudinaryService(Cloudinary cloudinary) {
-        this.cloudinary = cloudinary;
-    }
-
+    /**
+     * Sube un archivo a Cloudinary
+     * @param file Archivo a subir
+     * @param carpeta Carpeta en la que se guardará el archivo
+     * @return URL pública del archivo subido
+     * @throws IOException Si ocurre un error al subir el archivo
+     */
     public String uploadFile(MultipartFile file, String carpeta) throws IOException {
         if (file.isEmpty()) {
             throw new RuntimeException("El archivo está vacío");
@@ -29,6 +32,16 @@ public class CloudinaryService {
         } else {
             uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap("folder", "powerzone/" + carpeta));
         }
-        return uploadResult.get("url").toString(); // Retorna la URL pública del archivo
+        return uploadResult.get("public_id").toString();
+    }
+
+    /**
+     * Eliminar una imagen de Cloudinary.
+     * @param publicId El public_id de la imagen a eliminar.
+     * @return Un mapa con el resultado de la operación.
+     * @throws Exception Sí ocurre algún error durante la operación.
+     */
+    public Map deleteImage(String publicId) throws Exception {
+        return cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
     }
 }
