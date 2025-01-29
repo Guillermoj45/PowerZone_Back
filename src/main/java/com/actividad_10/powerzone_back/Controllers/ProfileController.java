b1dcef647c3a4e56fb31e8c2d014ca49283c3c0b
rafael <rcastanoblanca@safareyes.es>
2025-01-29 12:58:17 +0100

    busqueda realizada

--- src/main/java/com/actividad_10/powerzone_back/Controllers/ProfileController.java
+++ src/main/java/com/actividad_10/powerzone_back/Controllers/ProfileController.java
@@ -1,18 +1,28 @@
 package com.actividad_10.powerzone_back.Controllers;
 
 import com.actividad_10.powerzone_back.DTOs.Profile2Dto;
+import com.actividad_10.powerzone_back.DTOs.ProfileDto;
+import com.actividad_10.powerzone_back.Services.ProfileService;
 import com.actividad_10.powerzone_back.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/profile")
 public class ProfileController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    private final ProfileService profileService;
+
+    public ProfileController(UserService userService, ProfileService profileService) {
+        this.userService = userService;
+        this.profileService = profileService;
+    }
 
     @PostMapping("/getData")
     ResponseEntity<Profile2Dto> getProfile(@RequestHeader("Authorization") String token){
@@ -24,4 +34,15 @@ public class ProfileController {
         userService.updateProfile(token ,profile2Dto);
         return HttpStatus.OK;
     }
+
+    @GetMapping("/search")
+    public List<ProfileDto> searchUsers(@RequestParam String query) {
+        return profileService.searchUsers(query);
+    }
+
+    @GetMapping("/{id}")
+    public ProfileDto getUserById(@PathVariable Long id) {
+        return profileService.getUserById(id);
+    }
+
 }
--- src/main/java/com/actividad_10/powerzone_back/Repositories/ProfileRepository.java
+++ src/main/java/com/actividad_10/powerzone_back/Repositories/ProfileRepository.java
@@ -2,9 +2,11 @@ package com.actividad_10.powerzone_back.Repositories;
 
 import com.actividad_10.powerzone_back.Entities.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
+import java.util.List;
+
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
-
+    @Query("SELECT p FROM Profile p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<Profile> findByNameContainingIgnoreCase(String query);
 }
