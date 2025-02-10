2f366224b1d2565a21127cae2eef8988a7150f1b
Guillermo <guillermoremesa@gmail.com>
2025-02-10 09:16:25 +0100

    Consulta postDetails

--- src/main/java/com/actividad_10/powerzone_back/Services/MessageService.java
+++ src/main/java/com/actividad_10/powerzone_back/Services/MessageService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -46,4 +47,12 @@ public class MessageService {
         groupMessengerRepository.save(groupMessenger);
     }
 
+    
+    public List<GroupName> getGroups(Long userId){
+        return groupNameRepository.getGroupsByUserId(userId);
+    }
+
+    public List<GroupMessenger> getMessages(Long groupId){
+        return groupMessengerRepository.findByGrupouser_GroupId(groupId);
+    }
 }
