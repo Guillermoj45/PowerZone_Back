66ee82b3b30f26b14600ab292cb57166a7c0472e
Rufae <117822911+rufae@users.noreply.github.com>
2025-02-15 10:43:04 +0100

    recuperar ultimo mensaje de cada grupo

--- src/main/java/com/actividad_10/powerzone_back/DTOs/GrupoUltimoMensajeDTO.java
+++ src/main/java/com/actividad_10/powerzone_back/DTOs/GrupoUltimoMensajeDTO.java
@@ -1,20 +1,20 @@
 package com.actividad_10.powerzone_back.DTOs;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class GrupoUltimoMensajeDTO {
     private Long grupoId;
     private String grupoNombre;
-    private LocalDateTime ultimoMensajeTimestamp; // Cambia Long a LocalDateTime
+    private LocalDateTime ultimoMensajeTimestamp;
+    private String ultimoMensaje;
 
-    public GrupoUltimoMensajeDTO(Long grupoId, String grupoNombre, LocalDateTime ultimoMensajeTimestamp) {
+    public GrupoUltimoMensajeDTO(Long grupoId, String grupoNombre, LocalDateTime ultimoMensajeTimestamp, String ultimoMensaje) {
         this.grupoId = grupoId;
         this.grupoNombre = grupoNombre;
         this.ultimoMensajeTimestamp = ultimoMensajeTimestamp;
+        this.ultimoMensaje = ultimoMensaje;
     }
-
-    // Getters y Setters
-    public Long getGrupoId() { return grupoId; }
-    public String getGrupoNombre() { return grupoNombre; }
-    public LocalDateTime getUltimoMensajeTimestamp() { return ultimoMensajeTimestamp; }
 }
--- src/main/java/com/actividad_10/powerzone_back/Repositories/ChatMessageRepository.java
+++ src/main/java/com/actividad_10/powerzone_back/Repositories/ChatMessageRepository.java
@@ -10,12 +10,17 @@ import java.util.List;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<GroupMessenger, Long> {
-    @Query("SELECT NEW com.actividad_10.powerzone_back.DTOs.GrupoUltimoMensajeDTO(g.id, g.name, MAX(m.createdAt)) " +
+    @Query("SELECT NEW com.actividad_10.powerzone_back.DTOs.GrupoUltimoMensajeDTO( " +
+            "g.id, g.name, m.createdAt, m.message) " +
             "FROM GroupMessenger m " +
             "JOIN m.grupouser gu " +
             "JOIN gu.group g " +
-            "GROUP BY g.id, g.name")
+            "WHERE m.createdAt = (SELECT MAX(m2.createdAt) FROM GroupMessenger m2 " +
+            "                    JOIN m2.grupouser gu2 " +
+            "                    WHERE gu2.group.id = g.id) " +
+            "GROUP BY g.id, g.name, m.createdAt, m.message")
     List<GrupoUltimoMensajeDTO> obtenerUltimosMensajesPorGrupo();
+
 
 
 }
